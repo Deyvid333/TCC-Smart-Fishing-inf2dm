3642ac943f62fd75dfe495a7864cf4a3ebd306a9
Deyvid333 <rm94999@estudante.fieb.edu.br>
2026-04-08 02:31:04 -0300

    ligando back com o front

--- Back/smartfishingd-main-main/smartfishingd-main/src/main/java/com/itb/inf2dm/smartfishingd/CorsConfig.java
+++ Back/smartfishingd-main-main/smartfishingd-main/src/main/java/com/itb/inf2dm/smartfishingd/CorsConfig.java
@@ -14,14 +14,7 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/api/**")
-                        .allowedOrigins(
-                            "http://localhost:5173",
-                            "http://localhost:3000",
-                            "https://special-umbrella-r49pj9qjw7625gxg-5173.app.github.dev",
-                            "https://special-umbrella-r49pj9qjw7625gxg-5174.app.github.dev",
-                            "https://special-umbrella-r49pj9qjw7625gxg-5175.app.github.dev",
-                            "https://special-umbrella-r49pj9qjw7625gxg-5176.app.github.dev"
-                        )
+                        .allowedOriginPatterns("*")
                         .allowedMethods("GET", "POST", "PUT", "DELETE")
                         .allowedHeaders("*");
             }
